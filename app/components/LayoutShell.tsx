cfa69c5ad92eeb612ceb9e4bf87b95922822e171
Renita Hamilton <renita.hamilton@Mac.home.local>
2026-01-15 10:58:20 -0500

    fix: update LayoutShell to remove DashboardSidebar import

--- app/components/LayoutShell.tsx
+++ app/components/LayoutShell.tsx
@@ -2,7 +2,6 @@
 
 import React from 'react';
 import DashboardHeader from './DashboardHeader';
-import DashboardSidebar from './DashboardSidebar';
 
 interface LayoutShellProps {
   children: React.ReactNode;
@@ -12,14 +11,11 @@ export default function LayoutShell({ children }: LayoutShellProps) {
   return (
     <div className="min-h-screen bg-offWhite">
       <DashboardHeader />
-      <div className="flex">
-        <DashboardSidebar />
-        <main className="flex-1 overflow-y-auto">
-          <div className="container mx-auto py-8 px-6">
-            {children}
-          </div>
-        </main>
-      </div>
+      <main className="flex-1 overflow-y-auto">
+        <div className="container mx-auto py-8 px-6">
+          {children}
+        </div>
+      </main>
     </div>
   );
 }
