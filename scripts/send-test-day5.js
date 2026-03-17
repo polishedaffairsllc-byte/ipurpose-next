cd1b1be178aef47d423a2cb3b0b5e3b307e99b03
Renita Hamilton <renita.hamilton@Mac.home.local>
2026-03-17 18:14:04 -0400

    feat: restore Clarity Check bridge section to Day 5 email

--- scripts/send-test-day5.js
+++ scripts/send-test-day5.js
@@ -76,6 +76,20 @@ const htmlContent = `
 
           <hr class="divider">
 
+          <p>You took the Clarity Check.<br>
+          You saw something.<br>
+          A recognition.<br>
+          A shift.<br>
+          A sense that the life you&rsquo;ve been fitting yourself into might not actually be yours.</p>
+
+          <p>Most people ignore that.<br>
+          They go back to what&rsquo;s familiar.<br>
+          They tell themselves they&rsquo;ll figure it out later.</p>
+
+          <p>But if you&rsquo;re still here&hellip; something in you didn&rsquo;t let it go.</p>
+
+          <hr class="divider">
+
           <p>That&rsquo;s where the Starter Pack comes in.</p>
 
           <p>Not a course.<br>
